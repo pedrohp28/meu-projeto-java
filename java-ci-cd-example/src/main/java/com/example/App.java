defb1e456b40cbd21149f3a71119243f8fedd37f
pedrohp28 <pedrohp36@gmail.com>
2025-02-01 11:41:55 -0300

    Atualizando o código

--- java-ci-cd-example/src/main/java/com/example/App.java
+++ java-ci-cd-example/src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Atualizado com sucesso!" );
+        System.out.println( "Hello World 123!" );
     }
 }
